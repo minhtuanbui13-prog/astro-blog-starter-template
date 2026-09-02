a113bc998728210cc6c0fdd085a1338587addd55
minhtuanbui13-prog <minhtuan.bui13@gmail.com>
2026-09-02 23:18:27 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,8 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://buiminhtuan.io.vn",
-	integrations: [
-		mdx(),
-		sitemap({
-			prerender: true,
-		}),
-	],
+	output: "static",
+	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
